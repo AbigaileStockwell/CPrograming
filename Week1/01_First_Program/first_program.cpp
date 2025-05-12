2f388b126118eef3753d681ae8abbedf1ecf6b6a
AbigaileStockwell <astockwell9900@floridapoly.edu>
2025-05-12 17:50:44 +0000

    Create first program

---
+++ Week1/01_First_Program/first_program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// using directive
+// using namespace std;
+
+// using std::cout;
+
+int main() {
+
+    std::cout << "Hello COP3337!\n" << std::endl;
+
+    
+    // cout - console output
+    // endl - new line
+    // << stream insertion operator
+    // :: scope resolution operator
+    // std - standard namespace
+    // g++ - compiles code
+
+    std::cout << "5 / 2 is" << 5.0 / 2 << std::endl;
+
+
+    return 0;
+}
